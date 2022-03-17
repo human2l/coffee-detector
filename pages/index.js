4068fe2d8dfda90ddbf7688b976c6cc25c4adc66
Kai <human2lkai@gmail.com>
2022-03-17 12:32:32 +1100

    add styles to banner

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Detector</h1>
         <Banner buttonText="Show me" handleOnClick={handleOnBannerBtnClick} />
       </main>
     </div>
