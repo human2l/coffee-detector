d43f76ec753a9656f69b7f67cd3a4c9358911ea6
Kai <human2lkai@gmail.com>
2022-03-17 19:04:51 +1100

    add page /coffe-store

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const CoffeeStore = () => {
+  const router = useRouter();
+  return (
+    <div>
+      CoffeeStore {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default CoffeeStore;
