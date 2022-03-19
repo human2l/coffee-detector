6d924c2db85f6d3ee14ad5f4dfc762c50b19cc1f
Kai <human2lkai@gmail.com>
2022-03-19 11:36:09 +1100

    add coffee shop card style

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/banner";
+import Card from "../components/card";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -20,6 +21,44 @@ export default function Home() {
         <Banner buttonText="Show me" handleOnClick={handleOnBannerBtnClick} />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400}></Image>
+          <div className={styles.cardLayout}>
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+            <Card
+              name="Some Coffee"
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/some-coffee"
+              className={styles.card}
+            />
+          </div>
         </div>
       </main>
     </div>
