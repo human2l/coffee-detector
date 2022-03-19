b637778e906ae1c11650cfe6a28da2d4de62101a
Kai <human2lkai@gmail.com>
2022-03-19 14:05:58 +1100

    applied pre-rendering for coffee-shop pages

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,14 +1,40 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+export const getStaticProps = async (staticProps) => {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+};
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       CoffeeStore {router.query.id}
       <Link href="/">
         <a>Back to home</a>
       </Link>
+      <p>{props.coffeeStore.id}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 import Banner from "../components/banner";
 import Card from "../components/card";
 import styles from "../styles/Home.module.css";
-import coffeeStores from "../data/coffee-stores.json";
+import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps(context) {
   //fetch data here
   return {
     props: {
-      coffeeStores,
+      coffeeStores: coffeeStoresData,
     }, // will be passed to the page component as props
   };
 }
@@ -30,19 +30,24 @@ export default function Home(props) {
       <main className={styles.main}>
         <Banner buttonText="Show me" handleOnClick={handleOnBannerBtnClick} />
         <div className={styles.heroImage}>
-          <Image src="/static/hero-image.png" width={700} height={400}></Image>
-          <div className={styles.cardLayout}>
-            {props.coffeeStores.map((coffeeStore) => (
-              <Card
-                key={coffeeStore.id}
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl}
-                href={`/coffee-store/${coffeeStore.id}`}
-                className={styles.card}
-              />
-            ))}
-          </div>
+          <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Local stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => (
+                <Card
+                  key={coffeeStore.id}
+                  name={coffeeStore.name}
+                  imgUrl={coffeeStore.imgUrl}
+                  href={`/coffee-store/${coffeeStore.id}`}
+                  className={styles.card}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
